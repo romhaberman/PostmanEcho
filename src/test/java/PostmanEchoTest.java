ee1036151e54ba5f99429bbdec755c70120b014c
Roman Khabarov <habermanrom@gmail.com>
2024-08-04 20:29:33 +1000

    first commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+class PostmanEchoTest {
+    @Test
+    void shouldReturnSendData() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hi") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when().log().all()
+                .post("/post")
+// Проверки
+                .then().log().all()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo("Hi"));
+
+    }
+}
